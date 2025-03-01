1ffe55854a1f3b73da44068551834e94eafbb4ff
Mitcelll Johnson <91237766+Mitchell-jpg@users.noreply.github.com>
2025-03-01 02:32:00 -0500

    add pipe class, make pipes move.

--- src/main/java/floppybird/FloppyBird.java
+++ src/main/java/floppybird/FloppyBird.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -24,9 +25,10 @@ public class FloppyBird extends JPanel implements ActionListener, KeyListener {
     Image bottomPipeImage;
 
     Bird bird;
-    int velocityY = 0;
+    ArrayList<Pipe> pipes;
 
     Timer gameLoop;
+    Timer placePipesTimer;
     
     FloppyBird(){
         setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
@@ -35,19 +37,35 @@ public class FloppyBird extends JPanel implements ActionListener, KeyListener {
         addKeyListener(this);
 
         // load images
-        backgroundImage = new ImageIcon(getClass().getResource("./assets/flappybirdbg.png")).getImage();
-        birdImage = new ImageIcon(getClass().getResource("./assets/flappybird.png")).getImage();
-        topPipeImage = new ImageIcon(getClass().getResource("./assets/toppipe.png")).getImage();
-        bottomPipeImage = new ImageIcon(getClass().getResource("./assets/bottompipe.png")).getImage();
+        this.backgroundImage = new ImageIcon(getClass().getResource("./assets/flappybirdbg.png")).getImage();
+        this.birdImage = new ImageIcon(getClass().getResource("./assets/flappybird.png")).getImage();
+        this.topPipeImage = new ImageIcon(getClass().getResource("./assets/toppipe.png")).getImage();
+        this.bottomPipeImage = new ImageIcon(getClass().getResource("./assets/bottompipe.png")).getImage();
 
         // load bird
-        int birdX = BOARD_WIDTH / 8;
-        int birdY = BOARD_HEIGHT / 2;
-        bird = new Bird(birdX, birdY, 34, 24, birdImage);
+        int birdX = this.BOARD_WIDTH / 8;
+        int birdY = this.BOARD_HEIGHT / 2;
+        this.bird = new Bird(birdX, birdY, 34, 24, birdImage);
+        this.pipes = new ArrayList<>();
+
+        // pipes timer
+        this.placePipesTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                placePipes();
+            }
+        });
+        placePipesTimer.start();
 
         // init timer
-        gameLoop = new Timer(1000/60, this);
-        gameLoop.start(); 
+        this.gameLoop = new Timer(1000/60, this);
+        this.gameLoop.start(); 
+    }
+
+    public void placePipes() {
+        Pipe pipe = new Pipe(this.BOARD_WIDTH, 0, 64, 512, this.topPipeImage);
+        pipe.y = (int)(0 - pipe.height / 4 - Math.random()*(pipe.height/2));
+        pipes.add(pipe);
     }
 
     @Override
@@ -63,12 +81,21 @@ public class FloppyBird extends JPanel implements ActionListener, KeyListener {
     }
 
     public void draw(Graphics g){
-        g.drawImage(backgroundImage, 0, 0, BOARD_WIDTH, BOARD_HEIGHT, null );
+        g.drawImage(this.backgroundImage, 0, 0, this.BOARD_WIDTH, this.BOARD_HEIGHT, null );
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
     }
 
     public void update(){
         bird.update();
+
+        for (Pipe pipe : this.pipes) {
+            pipe.update();
+        }
     }
 
     @Override
---
+++ src/main/java/floppybird/Pipe.java
@@ -0,0 +1,26 @@
+package floppybird;
+
+import java.awt.Image;
+
+public class Pipe {
+    int x;
+    int y;
+    int width;
+    int height;
+    Image img;
+
+    private boolean passed = false;
+    int velocity = 2;
+
+    Pipe(int xCord, int yCord, int width, int height, Image img){
+        this.x = xCord;
+        this.y = yCord;
+        this.width = width;
+        this.height = height;
+        this.img = img;
+    }
+
+    public void update() {
+        this.x -= this.velocity; // Move the pipe to the left
+    }
+}
